fa70cbd6712589931afc3f323ee73176a8ef7feb
Rustam <geforcegame0912@gmail.com>
2023-11-26 21:43:02 +0300

    ddwerw

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,21 +4,7 @@ import Icone_1 from "../img/icone1.svg"
 import Icone_2 from "../img/icone2.svg"
 import Icone_3 from "../img/icone3.svg"
 import Footer from "./Footer";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-
-import '../styles/swiper2.css';
-
-// import required modules
-import { Navigation } from 'swiper/modules';
-
 function Services () {
-
-
     function clickBtn1() {
         document.getElementById('frames').className = 'Service-frames'; 
       }
@@ -48,12 +34,44 @@ function Services () {
             border: solid 2px rgb(255, 95, 95);
             `;             
         }
-
     }
-    const [frameInputValue1, setFrameInputValue1] = useState('')
-    const [frameInputValue2, setFrameInputValue2] = useState('')
-    
-        
+    const [frameInputValue1, setFrameInputValue1] = useState('');
+    const [frameInputValue2, setFrameInputValue2] = useState('');
+    const [cardsIcone, setCardsIcone] = useState(Icone_1);
+    const [cardsTitle, setCardsTitle] = useState("Work and Travel Greenheart")
+    const [cardsBody, setCardsBody] = useState(
+        `sit amet consectetur. Adipiscing sed nulla nibh vulputate orci faucibus neque ultrices sapien. Neque in curabitur suscipit ut.
+                    Eu malesuada diam proin faucibus in leo non diam. Fermentum sed augue felis tortor id nam ipsum enim scelerisque. Proin aliquam in malesuada pretium elementum. Rutrum placerat urna velit elementum ornare vitae mauris in. Dolor amet sed et egestas at mollis quam enim.
+                    Massa nullam mi posuere in interdum facilisis dictum adipiscing. In at sed pulvinar sit vestibulum. Ullamcorper ut nisl in at sollicitudin lectus massa suscipit tincidunt. Etiam in sed praesent tristique. Sagittis scelerisque quis mattis purus.`
+    );
+    const cardsClick1 = () => {
+        setCardsIcone(Icone_1);
+        setCardsTitle("Work and Travel Greenheart");
+        setCardsBody(`
+        sit amet consectetur. Adipiscing sed nulla nibh vulputate orci faucibus neque ultrices sapien. Neque in curabitur suscipit ut.
+        Eu malesuada diam proin faucibus in leo non diam. Fermentum sed augue felis tortor id nam ipsum enim scelerisque. Proin aliquam in malesuada pretium elementum. Rutrum placerat urna velit elementum ornare vitae mauris in. Dolor amet sed et egestas at mollis quam enim.
+        Massa nullam mi posuere in interdum facilisis dictum adipiscing. In at sed pulvinar sit vestibulum. Ullamcorper ut nisl in at sollicitudin lectus massa suscipit tincidunt. Etiam in sed praesent tristique. Sagittis scelerisque quis mattis purus.
+        `)
+    }
+    const cardsClick2 = () => {
+        setCardsIcone(Icone_2);
+        setCardsTitle("Work and Travel Greenheart");
+        setCardsBody(`
+        sit amet consectetur. Adipiscing sed nulla nibh vulputate orci faucibus neque ultrices sapien. Neque in curabitur suscipit ut.
+        Eu malesuada diam proin faucibus in leo non diam. Fermentum sed augue felis tortor id nam ipsum enim scelerisque. Proin aliquam in malesuada pretium elementum. Rutrum placerat urna velit elementum ornare vitae mauris in. Dolor amet sed et egestas at mollis quam enim.
+        Massa nullam mi posuere in interdum facilisis dictum adipiscing. In at sed pulvinar sit vestibulum. Ullamcorper ut nisl in at sollicitudin lectus massa suscipit tincidunt. Etiam in sed praesent tristique. Sagittis scelerisque quis mattis purus.
+        `)
+    }
+    const cardsClick3 = () => {
+        setCardsIcone(Icone_3);
+        setCardsTitle("Work and Travel Greenheart");
+        setCardsBody(`
+        sit amet consectetur. Adipiscing sed nulla nibh vulputate orci faucibus neque ultrices sapien. Neque in curabitur suscipit ut.
+        Eu malesuada diam proin faucibus in leo non diam. Fermentum sed augue felis tortor id nam ipsum enim scelerisque. Proin aliquam in malesuada pretium elementum. Rutrum placerat urna velit elementum ornare vitae mauris in. Dolor amet sed et egestas at mollis quam enim.
+        Massa nullam mi posuere in interdum facilisis dictum adipiscing. In at sed pulvinar sit vestibulum. Ullamcorper ut nisl in at sollicitudin lectus massa suscipit tincidunt. Etiam in sed praesent tristique. Sagittis scelerisque quis mattis purus.
+        `)
+    }
+ 
     return(
         <div className="MAIN">
             <div className="frame_none" id='frames'>
@@ -74,61 +92,55 @@ function Services () {
             </div>
         <div className="main_services_cards">
         <div className="services_cards">
-                <Cards content={{
-                id: "Icone_1",
-                class: "cards-footer1 cards-f cards-serv",
-                title: "Work and Travel Greenheart",
-                body: "Трудоустройство из базы работодателей Greenheart",
-                src: Icone_1,
-                price: "2500$",
-                link: "Подробнее",
-                href: "/Services"
-                }}/>
-
-                <Cards content={{
-                    id: "Icone_2",
-                class: "cards-footer2 cards-f cards-serv",
-                title: "Work and Travel Greenheart",
-                body: "Трудоустройство от DagCorporation! Предоставление жилья, встреча в аэропорту, дополнительная поддержка на терретории Америки",
-                src: Icone_2,
-                price: "2500$",
-                link: "Подробнее",
-                href: "/Services"
-                }}/>
-
-                <Cards content={{
-                    id: "Icone_3",
-                class: "cards-footer3 cards-f cards-serv",
-                title: "Work and Travel Greenheart",
-                body: "Трудоустройство от DagCorporation! Предоставление жилья, встреча в аэропорту, дополнительная поддержка на терретории Америки",
-                src: Icone_3,
-                price: "2500$",
-                link: "Подробнее",
-                href: "/Services"  
-                }}/>
-        </div>
-
-            <div className="large_card">
-            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                <SwiperSlide>
+                <div className="services_cards_click" id="cards_click" onClick={cardsClick1}>
                     <Cards content={{
+                    id: "Icone_1",
+                    class: "cards-footer1 cards-f cards-serv",
+                    title: "Work and Travel Greenheart",
+                    body: "Трудоустройство из базы работодателей Greenheart",
+                    src: Icone_1,
+                    price: "2500$",
+                    link: "Подробнее",
+                    href: "/Services"
+                    }}/>
+                </div>
+                <div className="services_cards_click" id="cards_click" onClick={cardsClick2}>
+                    <Cards content={{
+                    id: "Icone_2",
+                    class: "cards-footer2 cards-f cards-serv",
+                    title: "Work and Travel Greenheart",
+                    body: "Трудоустройство от DagCorporation! Предоставление жилья, встреча в аэропорту, дополнительная поддержка на терретории Америки",
+                    src: Icone_2,
+                    price: "2500$",
+                    link: "Подробнее",
+                    href: "/Services"
+                    }}/>
+                </div>
+                <div className="services_cards_click" id="cards_click" onClick={cardsClick3}>
+                    <Cards content={{
+                    id: "Icone_3",
+                    class: "cards-footer3 cards-f cards-serv",
+                    title: "Work and Travel Greenheart",
+                    body: "Трудоустройство от DagCorporation! Предоставление жилья, встреча в аэропорту, дополнительная поддержка на терретории Америки",
+                    src: Icone_3,
+                    price: "2500$",
+                    link: "Подробнее",
+                    href: "/Services"  
+                    }}/>
+                </div>
+        </div>
+            <div className="large_card">
+            <Cards content={{
                     id: "Icone_1",
                     btn_id: "btn1",
                     class: "cards-footer1 cards-f cards-serv1",
-                    title: "Work and Travel Greenheart",
-                    body: `sit amet consectetur. Adipiscing sed nulla nibh vulputate orci faucibus neque ultrices sapien. Neque in curabitur suscipit ut.
-                    Eu malesuada diam proin faucibus in leo non diam. Fermentum sed augue felis tortor id nam ipsum enim scelerisque. Proin aliquam in malesuada pretium elementum. Rutrum placerat urna velit elementum ornare vitae mauris in. Dolor amet sed et egestas at mollis quam enim.
-                    Massa nullam mi posuere in interdum facilisis dictum adipiscing. In at sed pulvinar sit vestibulum. Ullamcorper ut nisl in at sollicitudin lectus massa suscipit tincidunt. Etiam in sed praesent tristique. Sagittis scelerisque quis mattis purus.`,
-                    src: Icone_1,
+                    title: cardsTitle,
+                    body: cardsBody,
+                    src: cardsIcone,
                     price: "Учавствовать",
                     link: "Цена: 2500$",
                     onclick: clickBtn1
                     }}/>
-                </SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-            </Swiper>
-                
             </div>
         </div>
 
